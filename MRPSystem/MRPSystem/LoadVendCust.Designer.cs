00f0b8278c31acd497fc40d1e8204a252f508359
Jaylee345 <120148962+Jaylee345@users.noreply.github.com>
2022-12-09 16:36:11 +0800

    add
    updated.

---
+++ MRPSystem/MRPSystem/LoadVendCust.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace MRPSystem
+{
+    partial class LoadVendCust
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(361, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecta file...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.filePath.Location = new System.Drawing.Point(50, 48);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(287, 30);
+            this.filePath.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(50, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "匯入資料";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(722, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(12, 264);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(722, 331);
+            this.textBox2.TabIndex = 4;
+            // 
+            // LoadVendCust
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 607);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.button1);
+            this.Name = "LoadVendCust";
+            this.Text = "LoadVendCust";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ MRPSystem/MRPSystem/LoadVendCust.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MRPSystem
+{
+    public partial class LoadVendCust : Form
+    {
+        public LoadVendCust()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    filePath.Text = openFileDialog1.FileName; 
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var e1= Encoding.Default;
+            string txtfile = openFileDialog1.FileName;
+            List<string> inssqls = new List<string>();
+            if (File.Exists(txtfile))
+            {
+                using (StreamReader sr = new StreamReader( txtfile, Encoding.Default))
+                {
+                    string line;
+                    int i1 = 0,i2=0;
+                    string ColumnN ="";
+                    int[] colindex = new int[50];
+                    string[] data = new string[50];
+                    line = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        string[] ChtTitle = line.Split(',');
+                        if (ChtTitle != null) 
+                        {
+                            foreach (var item in ChtTitle) 
+                            {
+                                string[] EngTitle = item.Split('-');
+                                if (EngTitle != null && EngTitle.Length >1) 
+                                {
+                                    ColumnN += EngTitle[1] + ",";
+                                    colindex[i2] = i1;
+                                    Console.WriteLine(EngTitle[1] +"," );
+                                    i2 += 1;
+                                }
+                                i1 +=1;
+                            }
+                        }
+
+
+                    }
+                    textBox1.Text = ColumnN.Substring(0, ColumnN.Length-1);
+                    for(int c1=0;c1<i2;c1++)
+                        textBox2.Text +=  colindex[c1] + "_";
+
+                    string sql1 = "", sql = "";
+                    sql = "insert into aacvned(" + ColumnN.Substring(0, ColumnN.Length - 1) + ") values({0})";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        sql1 = "";
+                        if (!string.IsNullOrEmpty(line)) 
+                        {
+                            data = line.Split(',');
+                            if (data[0].Substring(0, 1) == "C") 
+                            {
+                                for (int c1 = 0; c1 < i2; c1++)
+                                    sql1 += string.Format("'{0}',", data[c1]);
+                            }
+                            if(!string.IsNullOrEmpty(sql1))
+                                inssqls.Add(string.Format(sql, sql1));
+                        }
+                        //    string id = line.Substring(0, 6);
+                        
+
+                    }
+                }
+                foreach (var t1 in inssqls)
+                    textBox2.Text += t1 + Environment.NewLine;
+            }
+        }
+    }
+}
