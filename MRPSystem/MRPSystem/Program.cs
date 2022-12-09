145f5aef6077a76df30303d7bac22849fc29b7db
Jaylee345 <120148962+Jaylee345@users.noreply.github.com>
2022-12-09 16:34:04 +0800

    updated.

--- MRPSystem/MRPSystem/Program.cs
+++ MRPSystem/MRPSystem/Program.cs
@@ -16,7 +16,8 @@ namespace MRPSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuForm());
+            //Application.Run(new MenuForm());
+            Application.Run(new LoadVendCust());
         }
     }
 }
